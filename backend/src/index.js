10879c9a2ec969bea22b9d3fed538c9c6ee9a7f8
DAWestphal <56050283+DAWestphal@users.noreply.github.com>
2020-07-21 04:23:25 -0300

    BackEnd Finalizado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let routes = require('./routes')
 let app = express();
+let cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
  
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,23 +1,21 @@
 const express = require('express')
-const crypto = require('crypto')
-const connection = require('./database/connection')
+
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
 const routes = express.Router()
 
+routes.post('/sessions', SessionController.create)
 
-routes.post('/ongs', async (req, res) => {
-    const { name, email, whatsapp, city, uf} = req.body
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
-    const id = crypto.randomBytes(4).toString('HEX')
-    
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
-    return res.json({ id })
-})
+routes.get('/profile', ProfileController.index)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes
