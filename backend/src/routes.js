e5d301be529e5ca76ff7516b44fe6f9f145819fe
DAWestphal <56050283+DAWestphal@users.noreply.github.com>
2020-07-21 03:26:37 -0300

    SDAS

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const crypto = require('crypto')
+const connection = require('./database/connection')
 const routes = express.Router()
 
 
-routes.post('/ongs', (req, res) => {
+routes.post('/ongs', async (req, res) => {
     const { name, email, whatsapp, city, uf} = req.body
 
     const id = crypto.randomBytes(4).toString('HEX')
     
-    console.log(data)
-
-    return res.json()
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+    return res.json({ id })
 })
 
 module.exports = routes
